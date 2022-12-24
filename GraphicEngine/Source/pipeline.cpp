f074c8e75f1ff210ec2cdb6935e382c760901fcb
Loic-Lemarchand <loic.lemarchand@isen-ouest.yncrea.fr>
2022-12-23 21:17:25 -0500

    Fix Pipeline configuration

--- GraphicEngine/Source/pipeline.cpp
+++ GraphicEngine/Source/pipeline.cpp
@@ -62,6 +62,38 @@ namespace Lve {
 	{
 		PipelineConfigInfo configInfo{};
 
+		configInfo.inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+		configInfo.inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;//Every 3 vertices are connected into a seperate triangle
+		configInfo.inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
+
+		configInfo.viewport.x = 0.0f;
+		configInfo.viewport.y = 0.0f;//For the explanation of VIEWPORT and SCISSORS : https://vulkan-tutorial.com/Drawing_a_triangle/Graphics_pipeline_basics/Fixed_functions#page_Viewports-and-scissors
+		configInfo.viewport.width = static_cast<float>(width);
+		configInfo.viewport.height = static_cast<float>(height);
+		configInfo.viewport.minDepth = 0.0f;
+		configInfo.viewport.maxDepth = 1.0f;
+
+		configInfo.scissor.offset = {0, 0};
+		configInfo.scissor.extent = { width, height };
+
+		configInfo.viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+		configInfo.viewportInfo.viewportCount = 1;
+		configInfo.viewportInfo.pViewports = &configInfo.viewport;
+		configInfo.viewportInfo.scissorCount = 1;
+		configInfo.viewportInfo.pScissors = &configInfo.scissor;
+
+		configInfo.rasterizationInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+		configInfo.rasterizationInfo.depthClampEnable = VK_FALSE;
+		configInfo.rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
+		configInfo.rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
+		configInfo.rasterizationInfo.lineWidth = 1.0f;
+		configInfo.rasterizationInfo.cullMode = VK_CULL_MODE_NONE;
+		configInfo.rasterizationInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+		configInfo.rasterizationInfo.depthBiasEnable = VK_FALSE;
+		configInfo.rasterizationInfo.depthBiasConstantFactor = 0.0f; //Optional
+		configInfo.rasterizationInfo.depthBiasClamp = 0.0f; //Optional
+		configInfo.rasterizationInfo.depthBiasSlopeFactor = 0.0f; //Optional
+		
 		return configInfo;
 	}
 
